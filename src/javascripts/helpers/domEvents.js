1bce60b77aba83b423577640707eeca31f65412f
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-07 15:43:50 -0600

    deleting pins

--- src/javascripts/helpers/domEvents.js
+++ src/javascripts/helpers/domEvents.js
@@ -1,12 +1,12 @@
 import showPins from '../components/pins';
 import boardPinsInfo from './data/boardPinData';
 import boardInfo from '../components/boardInfo';
+// import { deletePins } from './data/pinData';
 
 // will need to add uid as param
 const domEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
     if (e.target.id.includes('board-title')) {
-      console.warn('click');
       const boardId = e.target.id.split('--')[1];
       console.warn(boardId);
       boardPinsInfo(boardId).then((boardInfoObj) => {
@@ -15,9 +15,14 @@ const domEvents = () => {
       });
     }
 
-    if (e.target.id.includes('delete-board')) {
-      console.warn('click');
-    }
+    // if (e.target.id.includes('delete-pin')) {
+    //   console.warn('click');
+    //   if (window.customConfirm('Are you sure you want to delete?')) {
+    //     const firebaseKey = e.target.id.split('--')[1];
+    //     console.warn(firebaseKey);
+    //     deletePins(firebaseKey).then((pin) => console.warn(pin));
+    //   }
+    // }
   });
 };
 
