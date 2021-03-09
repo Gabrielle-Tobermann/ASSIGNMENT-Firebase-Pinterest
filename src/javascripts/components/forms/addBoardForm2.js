36a7a83b4cc06b3f649f71c6f809454e2e781850
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-08 19:27:20 -0600

    made new form

---
+++ src/javascripts/components/forms/addBoardForm2.js
@@ -0,0 +1,20 @@
+const addBoardForm = () => {
+  document.querySelector('#content-container').innerHTML = '';
+  document.querySelector('#add-button').innerHTML = '';
+  document.querySelector('#form-container').innerHTML = `<form>
+  <div class="form-group">
+    <label for="boardTitle">Board Title</label>
+    <input type="text" class="form-control" id="title-board" aria-describedby="boardTitle">
+  </div>
+  <div class="form-group">
+    <label for="boardImage">Image</label>
+    <input type="url" class="form-control" id="board-image">
+  </div>
+  <button type="submit" class="btn btn-primary" id="submit-board">Create New Board</button>
+</form>`;
+};
+
+// test changes
+// test 
+
+export default addBoardForm;
