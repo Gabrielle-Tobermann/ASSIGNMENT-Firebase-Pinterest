47f37ac347cb4cdc5823ec6b66c99cd81dd69457
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-11 21:23:58 -0600

    styled board cards

--- src/javascripts/components/Boards.js
+++ src/javascripts/components/Boards.js
@@ -3,12 +3,12 @@ const showBoards = (arr) => {
   document.querySelector('#form-container').innerHTML = '';
   document.querySelector('#add-button').innerHTML = '<div class="dropdown"><button class="btn btn-white shadow-none dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">+</button><div class="dropdown-menu" aria-labelledby="dropdownMenuButton"><a class="dropdown-item" href="#" id="create-board-btn">Board</a><a class="dropdown-item" href="#" id="create-pin-btn">Pin</a></div></div>';
   arr.forEach((item) => {
-    document.querySelector('#content-container').innerHTML += `<div class="card" style="width: 18rem;">
-    <img src="${item.image}" class="card-img-top" alt="${item.title}">
+    document.querySelector('#content-container').innerHTML += `<div class="card mb-5 border-0" style="width: 18rem;">
+    <img src="${item.image}" style="height: 360px"class="card-img-top" alt="${item.title}">
     <div class="card-body">
-    <a href="#"><h5 id="board-title--${item.firebaseKey}" class="card-title">${item.title}</h5></a>
-    <button class="btn btn-danger" data-toggle="modal" data-target="#formModal" id="edit-board--${item.firebaseKey}">Edit Board</button>
-      <button class="btn btn-danger" id="delete-board--${item.firebaseKey}">Delete Board</button>
+    <a href="#"><h5 class="text-dark font-weight-bold" id="board-title--${item.firebaseKey}" class="card-title">${item.title}</h5></a>
+    <button class="btn btn-outline-dark rounded-pill font-weight-bold" data-toggle="modal" data-target="#formModal" id="edit-board--${item.firebaseKey}">Edit Board</button>
+      <button class="btn btn-outline-dark rounded-pill font-weight-bold" id="delete-board--${item.firebaseKey}">Delete Board</button>
     </div>
   </div>`;
   });
--- src/javascripts/components/pins.js
+++ src/javascripts/components/pins.js
@@ -1,7 +1,7 @@
 const showPins = (arr) => {
   document.querySelector('#content-container').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
-  document.querySelector('#add-button').innerHTML = '<div class="dropdown"><button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Create</button><div class="dropdown-menu" aria-labelledby="dropdownMenuButton"><a class="dropdown-item" href="#" id="create-board-btn">Board</a><a class="dropdown-item" href="#" id="create-pin-btn">Pin</a></div></div>';
+  document.querySelector('#add-button').innerHTML = '<div class="dropdown"><button class="btn btn-white shadow-none dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">+</button><div class="dropdown-menu" aria-labelledby="dropdownMenuButton"><a class="dropdown-item" href="#" id="create-board-btn">Board</a><a class="dropdown-item" href="#" id="create-pin-btn">Pin</a></div></div>';
   arr.forEach((item) => {
     document.querySelector('#content-container').innerHTML += `<div class="card" style="width: 18rem;">
     <img src="${item.image}" class="card-img-top" alt="${item.title}">
